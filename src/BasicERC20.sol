284f8610aa4201988f6af96958dc9eadee7203f2
emanherawy <eman.herawy@gmaail.com>
2024-07-18 10:39:57 +0200

    add basic fuzz tests

---
+++ src/BasicERC20.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BasicERC20 is ERC20Capped, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(uint256 cap_) ERC20Capped(cap_) ERC20("Basic ERC20", "BSC") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    // function that only minter can call to mint tokens
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _mint(to, amount);
+    }
+
+
+    // function for user to mint token but max amount is 1000
+    function mintByUser(uint256 amount) public {
+        require(amount <= 1000, "Amount must be less than 1000");
+        _mint(msg.sender, amount);
+    }
+}
---
+++ test/BasicERC20.fuzz.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {BasicERC20} from "../src/BasicERC20.sol";
+
+contract BasicERC20Test is Test {
+
+    BasicERC20 public basicERC20;
+
+    function setUp() public {
+        basicERC20 = new BasicERC20(100000 ether);
+    }
+    // test mint function for minter role with fuzzing
+    function testFuzz_Mint(address to, uint256 amount) public {
+        vm.assume(to!=address(0));
+        vm.assume(amount <= 100000 ether);
+        basicERC20.mint(to, amount);
+        assertEq(basicERC20.balanceOf(to), amount);
+    }
+    function testFuzz_mintByUser( uint256 amount) public {
+        address user= address(0x123);
+        vm.startPrank(user);
+         vm.assume(amount <= 1000);
+        basicERC20.mintByUser( amount);
+        assertEq(basicERC20.balanceOf(user), amount);
+    }
+}
